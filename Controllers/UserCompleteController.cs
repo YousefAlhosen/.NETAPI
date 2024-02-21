24f137050fbdbf19ce881aa82601bea4ec2263f1
Yousef Alhsoen <yosefalhusen94@gmail.com>
2024-02-21 12:18:47 +0100

    This is the last modified edition

---
+++ Controllers/UserCompleteController.cs
@@ -0,0 +1,105 @@
+using System.Data;
+using Dapper;
+using DotnetAPI.Data;
+using DotnetAPI.Helpers;
+using DotnetAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetAPI.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("[controller]")]
+public class UserCompleteController : ControllerBase
+{
+    private readonly DataContextDapper _dapper;
+    private readonly ReusableSql _reusableSql;
+
+    public UserCompleteController(IConfiguration config)
+    {
+        _dapper =   new DataContextDapper(config);
+        _reusableSql = new ReusableSql(config);
+    }
+
+
+
+    [HttpGet("TestConnection")]
+    public DateTime TestConnection()
+    {
+        return _dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
+    }
+
+
+
+    [HttpGet("GetUsers/{userId}/{isActive}")]
+    public IEnumerable<UserComplete> GetUsers(int userId, bool isActive)
+    {
+        string sql = @"EXEC TutorialAppSchema.spUsers_Get";
+        string stringParameters = "";
+        DynamicParameters sqlParameters = new();
+
+        if (userId != 0){
+            stringParameters += ", @UserId=@UserIdParameter";
+            sqlParameters.Add("@UserIdParameter", userId, DbType.Int32);
+        }
+        if (isActive){
+            stringParameters += ", @Active=ActiveParameter";
+            //int activeValue = isActive ? 1 : 0;
+            sqlParameters.Add("@ActiveParameter", isActive, DbType.Boolean);
+        }
+
+        /* Check if the parameters string is not empty before extracting the substring.
+
+        if(!string.IsNullOrEmpty(stringParameters))
+        {
+            sql += stringParameters.Substring(1);
+
+        } */
+
+        if(stringParameters.Length > 0)
+        {
+            sql += stringParameters.Substring(1);
+        }
+
+        IEnumerable<UserComplete> users = _dapper.LoadDataWithParameters<UserComplete>(sql,sqlParameters);
+
+        return users;
+    }
+
+
+    [HttpPut("UpsertUser")]
+    public IActionResult UpsertUser(UserComplete user)
+    {
+
+        if (_reusableSql.UpsertUser(user))
+        {
+            return Ok();
+        }
+
+        throw new Exception(" Failed to update user");
+
+    }
+
+
+
+    [HttpDelete("DeleteUser/{userId}")]
+    public IActionResult DeleteUser(int userId)
+    {
+        string sql = @"EXEC TutorialAppSchema.spUser_Delete
+            @UserId =@UserIdParameter " ;
+
+        DynamicParameters sqlParameters = new();
+        sqlParameters.Add("@UserIdParameter", userId,DbType.Int32);
+
+
+        if (_dapper.ExecuteSqlWithParameters(sql, sqlParameters))
+        {
+            return Ok();
+        }
+
+        throw new Exception(" Failed to delete user");
+    }
+
+    
+}
---
+++ Models/UserComplete.cs
@@ -0,0 +1,30 @@
+namespace DotnetAPI.Models
+{
+    public partial class UserComplete
+    {
+        public int UserId {get; set;}
+        public string FirstName {get; set;}
+        public string LastName {get; set;}
+        public string Email {get; set;}
+        public string Gender {get; set;}
+        public bool Active {get; set;}
+        public string JobTitle {get; set;}
+        public string Department {get; set;}
+        public decimal Salary {get; set;}
+
+
+        public UserComplete()
+        {
+            FirstName ??="";
+            LastName ??="";
+            Email ??="";
+            Gender ??="";
+            JobTitle ??="";
+            Department ??="";
+        }
+        
+
+    }
+
+
+}
